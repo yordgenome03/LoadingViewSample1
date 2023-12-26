b3cc107254d00004595c0674bc03b21124a10312
tahara yo <tahara.you@ring-tech.jp>
2023-12-26 13:51:56 +0900

    Initial Commit

---
+++ LoadingViewSample1/LoadingViewSample1App.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingViewSample1App.swift
+//  LoadingViewSample1
+//
+//  Created by yotahara on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LoadingViewSample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
